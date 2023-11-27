bd4b2b3ea2a4f5519a408a257024c36334c458ca
dimitrii-ben <dimitrii.benoit@hotmail.com>
2023-11-27 19:55:21 +0100

    Question 6

--- TP/main.c
+++ TP/main.c
@@ -12,6 +12,7 @@
 #define EXIT "exit\n"
 #define INPUT_SIZE 128
 #define CLOCK_MONOTONIC 1
+#define PATH "/usr/bin/"
 int displayNewLine(int exit_status){
     return write(STDOUT_FILENO, SHELL_NAME, strlen(SHELL_NAME));  
 }
@@ -42,9 +43,35 @@ int initENSEASH() {
     
     return 0;
 }
+char** getArguments(char* raw_command,char* delimiter){
+    char* copy = malloc(strlen(raw_command));
+    strcpy(copy,raw_command);
+    char* token = strtok(copy,delimiter);
+    int token_count = 0;
+    //Counting the number of token
+    while(token !=NULL){
+        token_count++;
+        token=strtok(NULL,delimiter);
+    }
+    //Allocate memory for the array of strings and then adding each token to the array
+    char ** arr = (char**) malloc((token_count+1)*sizeof(char*));
+    int i = 0;
+    strcpy(copy,raw_command);
+    token = strtok(copy,delimiter);
 
+    while(token != NULL){
+        arr[i] = strdup(token);
+        token = strtok(NULL,delimiter);
+        i++;
+    }
+    arr[i-1][strlen(arr[i-1])-1] = '\0',
+    arr[i]=NULL;
+    return arr;
+
+}
 int main() {
-    char input[INPUT_SIZE],new_line_buffer[INPUT_SIZE];
+    char input[INPUT_SIZE],new_line_buffer[INPUT_SIZE],path[INPUT_SIZE];
+    char**arguments;
     int status,bytes_read;
     struct timespec time_result_before,time_result_after;
     if (initENSEASH() == -1) {
@@ -53,6 +80,7 @@ int main() {
     
     // Current shell while loop
     while (1) {
+        strcpy(path,PATH);
         resetBuffer(input);
 
         bytes_read = read(STDIN_FILENO,input,sizeof(input));
@@ -66,8 +94,10 @@ int main() {
             perror("Fork failed");
         }
         if (pid == 0) {
-            input[strlen(input)-1] = '\0';
-            execlp(input, input, NULL);
+            arguments = getArguments(input," ");
+            char* argu[] = {"ls","-l",NULL};
+            strcat(path,arguments[0]);
+            execv(path,arguments);
             // If execlp fails
             perror("enseash %%");
             exit(127);
