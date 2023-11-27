d3d2bdd35dc33034cd7eefd100cde493d487da92
dimitrii-ben <dimitrii.benoit@hotmail.com>
2023-11-27 14:33:12 +0100

    Adding time output to each new lines

--- TP/main.c
+++ TP/main.c
@@ -4,15 +4,20 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define WELCOME_MSG "Welcome to ENSEA Tiny Shell.\nPour quitter, tapez 'exit'.\n"
 #define SHELL_NAME "enseash %%"
-#define SHELL_NAME2 "enseash [exit:%d] %%"
+#define SHELL_NAME2 "enseash [exit:%d|%dms] %%"
 #define EXIT "exit\n"
 #define INPUT_SIZE 128
-
+#define CLOCK_MONOTONIC 1
 int displayNewLine(int exit_status){
     return write(STDOUT_FILENO, SHELL_NAME, strlen(SHELL_NAME));  
+}
+
+int getExecutionTime(struct timespec spec_before,struct timespec spec_after){
+    return (int)((spec_after.tv_sec) * 1000 + (spec_after.tv_nsec)/1000000)-((spec_before.tv_sec) * 1000 + (spec_before.tv_nsec)/10000000);
 }
 void resetBuffer(char*buffer){
     for(int i = 0;i<INPUT_SIZE;i++){
@@ -41,7 +46,7 @@ int initENSEASH() {
 int main() {
     char input[INPUT_SIZE],new_line_buffer[INPUT_SIZE];
     int status,bytes_read;
-    
+    struct timespec time_result_before,time_result_after;
     if (initENSEASH() == -1) {
         exit(1);
     }
@@ -49,7 +54,9 @@ int main() {
     // Current shell while loop
     while (1) {
         resetBuffer(input);
+
         bytes_read = read(STDIN_FILENO,input,sizeof(input));
+        clock_gettime(CLOCK_MONOTONIC,&time_result_before);
         if (!strcmp(input,EXIT) || bytes_read ==0){
             exit(EXIT_SUCCESS);
         }
@@ -71,10 +78,14 @@ int main() {
             waitpid(pid, &status, 0);
             if(WIFEXITED(status)){
                 int exit_code = WEXITSTATUS(status);
-                sprintf(new_line_buffer,SHELL_NAME2,exit_code);
+                clock_gettime(CLOCK_MONOTONIC,&time_result_after);
+                sprintf(new_line_buffer,SHELL_NAME2,exit_code,getExecutionTime(time_result_before,time_result_after));
+                
             }
             
         }
+        
+        
         write(STDOUT_FILENO, new_line_buffer, strlen(new_line_buffer));
         
     }
