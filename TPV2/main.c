f3fbdd36bb9be41a20d5f2407749f5ccb08f01c2
ensea <ensea@StudentLab>
2023-12-04 09:48:51 +0100

    Question 5

--- TPV2/main.c
+++ TPV2/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 #include <sys/wait.h>
 
 #include "constant_header.h"
@@ -9,8 +10,9 @@
 #include "prompt.h"
 
 int main (){
-	
 	int ctrld;
+	double ex_time=4;
+	struct timespec time1={0},time2={0};
 	pid_t pid;
 	int code;
 	char exit_code[BUFF_SIZE]="";
@@ -20,6 +22,7 @@ int main (){
 		write(STDOUT_FILENO,exit_code, strlen(exit_code));
 		write(STDOUT_FILENO,PROMPT_MESSAGEp2, strlen(PROMPT_MESSAGEp2));
 		ctrld=read(STDIN_FILENO,input_buffer,BUFF_SIZE);	// reading of the command enters by the user
+		clock_gettime(CLOCK_REALTIME,&time1);
 		input_buffer[strlen(input_buffer)-1]='\0'; // to delete the tap of enter at the end
 		if((strcmp(input_buffer,"exit")==0)||(ctrld==0)){ //EOF is zero in the read
 			write(STDOUT_FILENO, BYE_MESSAGE, strlen(BYE_MESSAGE));
@@ -32,19 +35,21 @@ int main (){
 			exit(EXIT_FAILURE);
 		}
 		else if (pid==0){ // if it is the child
-			prompt(input_buffer);
+			prompt(input_buffer,ex_time);
 			perror("enseash %");
 			exit(127);			
 }
 		else{
 			int status;
 			waitpid(pid,&status,0);	//waiting for the child process to be terminated
+					clock_gettime(CLOCK_REALTIME,&time2);
+			ex_time= (double) (time2.tv_nsec-time1.tv_nsec)/1000000; //(((double) time2.tv_nsec-time1.tv_nsec)*(1/1000));
 			if (WIFEXITED(status)){
 				code=WEXITSTATUS(status);
-				sprintf(exit_code, "[exit: %d]",code);
+				sprintf(exit_code, "[exit: %d| %f ms]",code,ex_time);
 			} else if (WIFSIGNALED(status)){
 				code=WTERMSIG(status);
-				sprintf(exit_code, "[signal: %d]",code);}
+				sprintf(exit_code, "[signal: %d| %f ms]",code,ex_time);}
 			}}
 	return 0;
 }
--- TPV2/prompt.c
+++ TPV2/prompt.c
@@ -3,16 +3,19 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <time.h>
 
 #include "constant_header.h"
 #include "prompt.h"
 
-void prompt(char* inp){
-			
-			if(strcmp(inp,"exit")!=0){
-				execlp(inp,inp,NULL);	//execute the command given in input
-			}
-			
+double prompt(char* inp,double dt){
+	
+	if(strcmp(inp,"exit")!=0){
+		execlp(inp,inp,NULL);	//execute the command given in input
+		
+		
+	}
+		
 }
 
 		
